11473843a152e7a66c10802a2be8b0cb4a37278a
Kareem-Bizreh <kareembezreh@gmail.com>
2024-06-04 18:29:04 +0300

    completing problem1

--- ITE N.K.O.M/src/ITENodesOfTrees/Controller/TreeFormatController.java
+++ ITE N.K.O.M/src/ITENodesOfTrees/Controller/TreeFormatController.java
@@ -4,65 +4,69 @@ import ITENodesOfTrees.Module.Node;
 import ITENodesOfTrees.View.*;
 
 public class TreeFormatController {
-    static int ch=0;
+    static int ch = 0;
     public static Node treeRoot;
-    public static void addNodeToTree(String name,int height,int width){
-        Node newNode=new Node(name, width, height);
-        if(treeRoot==null){
+
+    public static void addNodeToTree(String name, int height, int width) {
+        Node newNode = new Node(name, width, height);
+        if (treeRoot == null) {
             System.out.println("Root");
-            treeRoot=newNode;
-        }else{
-            ch=0;
-            find(treeRoot,null,name);
+            treeRoot = newNode;
+        } else {
+            ch = 0;
+            find(treeRoot, null, name);
         }
-      
+
         TreeFormatUI.treepanel.updateTree(treeRoot);
         Node.dfs(treeRoot);
     }
-    public static Node searchNode(Node root,Node father){
-        if(root==null){
+
+    public static Node searchNode(Node root, Node father) {
+        if (root == null) {
             return root;
-        }else{
-            if(father!=null){
+        } else {
+            if (father != null) {
                 System.out.println("H");
-                searchNode(root.leftChild,root);
-                if(!(root.value.equals("|")&&root.value.equals("-")&&root.value.equals("-")))
-                    searchNode(root.rightChild,root);
-                }
+                searchNode(root.leftChild, root);
+                if (!(root.value.equals("|") && root.value.equals("-") && root.value.equals("-")))
+                    searchNode(root.rightChild, root);
             }
-            return null; 
+        }
+        return null;
     }
-    
-    static void find(Node root,Node father,String v){
+
+    static void find(Node root, Node father, String v) {
         // if(father!=null)
         // if(!(father.value.equals("|")&&father.value.equals("-")&&father.value.equals("-"))){
-        //     return;
+        // return;
         // }
-        if(ch==1)return;
-        if(!(root.value.equals("|")||root.value.equals("-")||root.value.equals("-"))){
-           return;
+        if (ch == 1)
+            return;
+        if (!(root.value.equals("|") || root.value.equals("-") || root.value.equals("-"))) {
+            return;
         }
-        if(root.leftChild==null){
-            root.leftChild=new Node(v);
-            ch=1;
+        if (root.leftChild == null) {
+            root.leftChild = new Node(v);
+            ch = 1;
             return;
         }
         find(root.leftChild, root, v);
-        if(ch==0){
-            if(root.rightChild==null){
-                root.rightChild=new Node(v);
-                ch=1;
+        if (ch == 0) {
+            if (root.rightChild == null) {
+                root.rightChild = new Node(v);
+                ch = 1;
                 return;
             }
             find(root.rightChild, root, v);
         }
     }
-   public static boolean isEndTree(Node node){
-        
-            if(node.leftChild==null &&node.rightChild==null)
-                if(!(node.value.equals("|")||node.value.equals("-")||node.value.equals("-")))
-                    return true;
-            else{
+
+    public static boolean isEndTree(Node node) {
+
+        if (node.leftChild == null && node.rightChild == null)
+            if (!(node.value.equals("|") || node.value.equals("-") || node.value.equals("-")))
+                return true;
+            else {
                 return false;
             }
         return isEndTree(node.leftChild) && isEndTree(node.rightChild);
--- ITE N.K.O.M/src/ITENodesOfTrees/Module/RectangleNode.java
+++ ITE N.K.O.M/src/ITENodesOfTrees/Module/RectangleNode.java
@@ -5,43 +5,59 @@ import java.util.List;
 
 public class RectangleNode {
     List<Integer> Rectangles;
+    String value;
+    int width, height;
+    RectangleNode left, right;
 
-    public RectangleNode(int width, int height, List<Integer> rectangles) {
+    public RectangleNode(List<Integer> rectangles, String value, int width, int height, RectangleNode left,
+            RectangleNode right) {
         Rectangles = rectangles;
+        this.value = value;
         this.width = width;
         this.height = height;
+        this.left = left;
+        this.right = right;
     }
 
-    int width, height;
+    List<RectangleNode> collecting(RectangleNode left, RectangleNode right) {
 
-    RectangleNode collecting(RectangleNode left, RectangleNode right) {
         for (Integer Index : left.Rectangles) {
             if (right.Rectangles.contains(Index))
                 return null;
         }
         List<Integer> Index = new ArrayList<>();
+        List<RectangleNode> Rec = new ArrayList<>();
         Index.addAll(left.Rectangles);
         Index.addAll(right.Rectangles);
-        for (int i = 0; i < 2; i++) {
-            if (left.height == right.height) {
-                return new RectangleNode(left.width + right.width, left.height, Index);
-            }
-            if (left.width == right.width) {
-                return new RectangleNode(left.width, left.height + right.height, Index);
-            }
-            int tmp = left.width;
-            left.width = left.height;
-            left.height = tmp;
+        if (left.height == right.height) {
+            int height, width;
+            height = left.height;
+            width = left.width + right.width;
+            Rec.add(new RectangleNode(Index, "|", width, height, left, right));
+            // System.out.println("left : " + left.name + " " + left.width + " " +
+            // left.length);
+            // System.out.println("right : " + right.name + " " + right.width + " " +
+            // right.length);
         }
-        return null;
+        if (left.width == right.width) {
+            int height, width;
+            width = left.width;
+            height = left.height + right.height;
+            Rec.add(new RectangleNode(Index, "-", width, height, left, right));
+            // System.out.println("left : " + left.name + " " + left.width + " " +
+            // left.length);
+            // System.out.println("right : " + right.name + " " + right.width + " " +
+            // right.length);
+        }
+        return Rec;
     }
 
-    boolean Rectangle(List<Node> Rec) {
+    RectangleNode Rectangle(List<Node> Rec) {
         List<RectangleNode> Nodes = new ArrayList<>();
         for (int i = 0; i < Rec.size(); i++) {
             List<Integer> Index = new ArrayList<>();
             Index.add(i);
-            Nodes.add(new RectangleNode(Rec.get(i).width, Rec.get(i).height, Index));
+            Nodes.add(new RectangleNode(Index, Rec.get(i).value, Rec.get(i).width, Rec.get(i).height, null, null));
         }
         for (int i = 0; i < Nodes.size(); i++) {
             for (int j = 0; j < Nodes.size(); j++) {
@@ -49,16 +65,17 @@ public class RectangleNode {
                     continue;
                 if (j < i && Nodes.get(i).Rectangles.size() == Nodes.get(j).Rectangles.size())
                     continue;
-                RectangleNode node = collecting(Nodes.get(i), Nodes.get(j));
-                if (node == null)
+                List<RectangleNode> RecNodes = collecting(Nodes.get(i), Nodes.get(j));
+                if (RecNodes == null || RecNodes.isEmpty())
                     continue;
-                System.out.println(node.Rectangles + " " + node.width + " " + node.height);
-                if (node.Rectangles.size() == Rec.size())
-                    return true;
-                Nodes.add(node);
+                for (RectangleNode node : RecNodes) {
+                    if (node.Rectangles.size() == Rec.size())
+                        return node;
+                    Nodes.add(node);
+                }
             }
         }
-        return false;
+        return null;
     }
 
     int NumberOfRectangles(List<Node> Nodes) {
@@ -69,7 +86,7 @@ public class RectangleNode {
                 if (((1 << j) & i) > 0)
                     collection.add(Nodes.get(j));
             }
-            counter += (Rectangle(collection) ? 1 : 0);
+            counter += (Rectangle(collection) != null ? 1 : 0);
         }
         return counter;
     }
